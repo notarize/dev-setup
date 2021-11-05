c86805eb1100afb09a50b03a41c555e7fb1cff42
bradynotarize <88348856+bradynotarize@users.noreply.github.com>
2021-11-05 10:17:14 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,20 +2,37 @@
 
 set -e
 
+touch ~/.zshrc
+
 # Install XCode command line  tools
-xcode-select --install
+if ! command -v xcodebuild &> /dev/null
+then
+  echo "Installing Xcode Cli Tools"
+  sudo xcode-select --install
+  read -n 1 -s -r -p "Follow the dialogues to install xcode utilites. Once completed press any key to continue."
+else
+  echo "Xcode Cli Tools is already installed. Continuing . . ."
+fi
 
 # Install Brew
+echo "Installing Brew"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Install tools through Brew
-brew install git nvm jq gh heroku
+echo "Installing Git"
+brew install git jq gh
+
+# Install heroku
+echo "Installing Heroku"
+brew tap heroku/brew && brew install heroku
 
 # install nvm
+echo "Installing NVM"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 
 # Generate SSH key
-ssh-keygen -t rsa -b 4096
+echo "Generating SSH key"
+ssh-keygen -t rsa -b 4096 -C `whoami` -N ''
 
 # Upload key to github
 gh auth login -w 
