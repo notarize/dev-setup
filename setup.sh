90320ed514abd0f012c266ff7e361ba94ca7b5ee
Jon Roberts <jon@emptyflask.net>
2021-11-12 01:09:48 -0600

    ran the script and made updates

--- setup.sh
+++ setup.sh
@@ -4,9 +4,6 @@ set -e
 
 touch ~/.zshrc
 
-mkdir -p ~/dev/notarize
-cd ~/dev/notarize
-
 # Verify preliminary requirements
 while true; do
     echo "Preliminary Requirements:"
@@ -27,7 +24,7 @@ read current_useremail
 if ! command -v brew &> /dev/null
 then
   echo "Installing Brew"
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
   echo "Brew is already installed. Continuing . . ."
 fi
@@ -36,12 +33,12 @@ fi
 if ! command -v git &> /dev/null
 then
   echo "Installing Git"
-  brew install git jq gh docker
-  git config --global user.name "`id -F`" 
+  brew install git
+  git config --global user.name "`id -F`"
   git config --global user.email "$current_useremail"
 else
   echo "Git is already installed. Continuing . . ."
-fi  
+fi
 
 # Install heroku
 if ! command -v heroku &> /dev/null
@@ -50,8 +47,23 @@ then
   brew tap heroku/brew && brew install heroku
 else
   echo "Heroku is already installed. Continuing . . ."
-fi  
-  
+fi
+
+# Docker
+if [[ ! -d "/Applications/Docker.app" ]]; then
+  echo "Installing Docker"
+  brew install homebrew/cask/docker
+  # Avoid asking the user if it's ok to run Docker.app
+  xattr -dr com.apple.quarantine /Applications/Docker.app
+  open --background /Applications/Docker.app
+else
+  echo "Docker is already installed. Continuing . . ."
+fi
+
+# Misc stuff
+echo "Installing miscellaneous other tools"
+brew install jq gh
+
 # install nvm
 if ! command -v node &> /dev/null
 then
@@ -66,31 +78,28 @@ if [ -f ~/.ssh/id_rsa ]; then
     echo "SSH key already exists. Continuing . . . "
 else
   echo "Generating SSH key"
-  ssh-keygen -t rsa -b 4096 -C "$current_usermail" -N ''
+  ssh-keygen -t rsa -b 4096 -C "$current_usermail" -N '' -f ~/.ssh/id_rsa
   echo "Host *" > ~/.ssh/config
   echo " AddKeysToAgent yes" >> ~/.ssh/config
   echo " UseKeychain yes" >> ~/.ssh/config
   echo " IdentityFile ~/.ssh/id_rsa" >> ~/.ssh/config
-
-ssh-add -K ~/.ssh/id_rsa
 fi
+
+ssh-add --apple-use-keychain ~/.ssh/id_rsa
 
 # Upload ssh key to github
 # TODO, add some instruction to do this manually if stuck on password prompt in browser
 echo "Authenticate into Github and upload SSH key into account."
-gh auth login -s write:public_key
+# TODO Force sign-in to github with Okta URL
+gh config set -h github.com git_protocol ssh
+gh auth login -h github.com -s admin:public_key
+gh ssh-key add ~/.ssh/id_rsa.pub -t `id -un`
 
 # clone bootstrap repo
 echo "Downloading bootstrap repo from Github"
-git clone git@github.com:notarize/bootstrap.git
+git clone git@github.com:notarize/bootstrap.git ~/dev/notarize/bootstrap
 
-read -n 1 -s -r -p "Initial Setup Complete. Next we will runbootstrap/bin/dev_setup.sh. Press any key to continue."
+read -n 1 -s -r -p "Initial Setup Complete. Next we will run bootstrap/bin/dev_setup.sh. Press any key to continue."
 
-cd ./bootstrap
+cd ~/dev/notarize/bootstrap
 ./bin/dev_setup.sh
-
-
-
-
-
-
